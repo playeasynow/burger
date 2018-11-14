81c01ab3136304615bbee43b9fd22d820d9cd2c5
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-11-14 15:17:22 -0600

    added to config files

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+require('./connection');
+
+const sqlCommands = {
+    selectAll: function() {
+        // code for method
+    },
+    insertOne: function() {
+        // code for method
+    },
+    updateOne: function() {
+        // code for method
+    }
+};
+
+module.exports = sqlCommands;
